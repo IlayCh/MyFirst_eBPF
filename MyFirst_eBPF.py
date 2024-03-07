880950812237a39c41108a627d5df2da78fd9c04
IlayCh <90407670+IlayCh@users.noreply.github.com>
2024-03-07 02:42:44 +0200

    Update MyFirst_eBPF.py

--- MyFirst_eBPF.py
+++ MyFirst_eBPF.py
@@ -1,13 +1,9 @@
-#!/usr/bin/env python3
-
 from bcc import BPF
 from pathlib import Path
-
 
 def process_event(cpu, data, size):
     event = bpf["events"].event(data)
     print(f"Process {event.comm.decode()} (PID: {event.pid}, PPID: {event.ppid}) called sys_clone")
-
 
 bpf_source = Path('MyFirst_eBPF.c').read_text()
 bpf = BPF(text=bpf_source)
